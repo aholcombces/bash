753ca32110d42f0b4aee1b3d904e9c0cea7a6c51
Andrew Holcomb <andrew.holcomb@theceshop.com>
2020-02-13 03:40:30 +0000

    refactored

--- leapdb_modify.sh
+++ leapdb_modify.sh
@@ -13,8 +13,8 @@ echo "Date ran: $(date)"
 echo
 
 dbname=$1
+newdb=$2
 latestsnapshot=$(aws rds describe-db-snapshots --db-instance-identifier=$dbname --query="reverse(sort_by(DBSnapshots, &SnapshotCreateTime))[0] | DBSnapshotIdentifier" --output text)
-#latestsnapshot=$(aws rds describe-db-snapshots --db-instance-identifier=$dbname --query="reverse(sort_by(DBSnapshots, &SnapshotCreateTime))[0] | DBSnapshotArn" --output text)
 
 # Check database status
 echo "Database status..."
@@ -30,26 +30,26 @@ aws rds describe-db-snapshots --db-snapshot-identifier $latestsnapshot | grep -i
 aws rds describe-db-snapshots --db-snapshot-identifier $latestsnapshot | grep -i "Engine"
 aws rds describe-db-snapshots --db-snapshot-identifier $latestsnapshot | grep -i "Status"
 
-## Delete database instance #| --final-db-snapshot-identifier <value>
-#echo
-#echo "Deleting $dbname so restore can begin..."
-#aws rds delete-db-instance --db-instance-identifier $dbname --skip-final-snapshot | grep -i "DBInstanceStatus"
-#
-## Check if database instance has been removed from Console
-#echo
-#echo "Checking if database has been removed..."
-#echo "This process can take a while. Please wait."
-#echo
-#while :
-#do
-#    dboutput=$(aws rds describe-db-instances --query 'DBInstances[*].[DBName,DBInstanceIdentifier]' --filters Name=db-instance-id,Values=$dbname --output text)
-#    if  [ "$dboutput" == '' ]; then 
-#        echo "-------------------------"
-#        echo "$dbname has been removed."
-#        echo "-------------------------"
-#        break
-#    fi 
-#done
+# Delete database instance #| --final-db-snapshot-identifier <value>
+echo
+echo "Deleting $newdb so restore can begin..."
+aws rds delete-db-instance --db-instance-identifier $newdb --skip-final-snapshot | grep -i "DBInstanceStatus"
+
+# Check if database instance has been removed from Console
+echo
+echo "Checking if database has been removed..."
+echo "This process can take a while. Please wait."
+echo
+while :
+do
+    dboutput=$(aws rds describe-db-instances --query 'DBInstances[*].[DBName,DBInstanceIdentifier]' --filters Name=db-instance-id,Values=$newdb --output text)
+    if  [ "$dboutput" == '' ]; then 
+        echo "------------------------------"
+        echo "$newdb has been removed."
+        echo "------------------------------"
+        break
+    fi 
+done
 
 # Restore database from snapshot
 # --db-subnet-group-name
@@ -57,24 +57,24 @@ aws rds describe-db-snapshots --db-snapshot-identifier $latestsnapshot | grep -i
 # --availability-zone us-east-2b
 echo
 echo "Creating ces-leapdb-test from restored snapshot id: $latestsnapshot"
-aws rds restore-db-instance-from-db-snapshot --db-snapshot-identifier $latestsnapshot --db-subnet-group-name ces-prd-stack-dbsubnetgroup-f72doe140v1l --vpc-security-group-ids sg-03b91652619d09223 --db-instance-class db.t2.micro --db-instance-identifier 'ces-leapdb-test' | grep -i "DBInstanceStatus"
+aws rds restore-db-instance-from-db-snapshot --db-snapshot-identifier $latestsnapshot --db-subnet-group-name ces-prd-stack-dbsubnetgroup-f72doe140v1l --vpc-security-group-ids sg-03b91652619d09223 --db-instance-class db.t2.micro --db-instance-identifier $newdb | grep -i "DBInstanceStatus"
 echo
 
-## Check if new database is available
-#echo "Checking if $dbname is available and online..."
-#echo "This process can take a while. Please wait."
-#echo
-#while :
-#do
-#    dbstatus=$(aws rds describe-db-instances --db-instance-identifier $dbname | grep -i "DBInstanceStatus")
-#    dbavailable=$(aws rds describe-db-instances --db-instance-identifier $dbname | grep -i "DBInstanceStatus" | grep -i "available")
-#    if [[ "$dbstatus" == $dbavailable ]]; then
-#        echo "-----------------------------"
-#        echo "$dbname is available"
-#        echo "-----------------------------"
-#        break
-#    fi
-#done
+# Check if new database is available
+echo "Checking if ces-leapdb-test is available and online..."
+echo "This process can take a while. Please wait."
+echo
+while :
+do
+    dbstatus=$(aws rds describe-db-instances --db-instance-identifier $newdb | grep -i "DBInstanceStatus")
+    dbavailable=$(aws rds describe-db-instances --db-instance-identifier $newdb | grep -i "DBInstanceStatus" | grep -i "available")
+    if [[ "$dbstatus" == $dbavailable ]]; then
+        echo "-----------------------------"
+        echo "$newdb is available"
+        echo "-----------------------------"
+        break
+    fi
+done
 
 # Stop new database instance (if aim is to save cost in non-prod)
 #sleep 5
